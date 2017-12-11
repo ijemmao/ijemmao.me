65c404ad60b2bec1e38561d72b8bebe5998fac1e
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-10 23:09:10 -0800

    push included some starter files

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(8001, () => {
+  console.log("Listening on port 8001")
+})
