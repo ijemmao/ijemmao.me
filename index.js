767662b5b3ed370561010991277be15dc6d580ac
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-14 23:02:43 -0800

    able to use express to run website

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var app = express()
 var path = require('path')
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+app.use(express.static('public'));
 
 app.listen(8001, () => {
   console.log("Listening on port 8001")
