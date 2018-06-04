b0d4bcc5b043ccd3089ee7fbfe920cd21bc09a5d
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-06-03 19:55:10 -0700

    included project detailed pages

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ document.getElementById('experience-nav-button').onclick = (event) => {
     behavior: 'smooth'
   })
 }
+
+document.getElementById('pulse').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/pulse.html"
+}
+
+document.getElementById('hamono').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/hamono.html"
+}
+
+document.getElementById('seabird').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/seabird.html"
+}
+
+document.getElementById('waddupr').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/waddupr.html"
+}
+
+document.getElementById('waddupboard').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/waddupboard.html"
+}
+
+document.getElementById('needone').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/needone.html"
+}
+
+document.getElementById('purrpl').nextSibling.nextSibling.onclick = (event) => {
+  window.location.href = "./projects/purrpl.html"
+}
