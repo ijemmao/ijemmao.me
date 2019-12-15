9fa690226f1299cc84f997bf1fecb6f01764e5e2
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-12-15 14:56:16 -0500

    Finish project preview selection animation

--- src/components/projectPreview/projectPreview.js
+++ src/components/projectPreview/projectPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createRef } from 'react';
 import { unmountComponentAtNode } from 'react-dom';
 import $ from 'jquery';
 import timetracker from '../../images/timetracker.png';
@@ -15,6 +15,7 @@ const ProjectPreview = ({
   position,
   id
 }) => {
+  const [ref] = useState(createRef());
   const [hover, setHover] = useState(false);
   
   const handleMouseEnter = (e) => setHover(true);
@@ -33,17 +34,37 @@ const ProjectPreview = ({
       const blogPostContent = document.createElement('div');
       blogPostContent.classList.add('blog-post-content');
       // Updates DOM
-      $(main.firstChild).fadeOut(300, () => {
-        unmountComponentAtNode(main);
+      
+      $('.projectPreviewContainer').not(`#${ref.current.id}`).animate({ left: '-100vw' }, 800).promise().then(() => {
+      
+        $(`#${ref.current.id} > .mask`).animate({ height: 0 }, 800);
+
+        const projectPreview = $(`#${ref.current.id}`);
+        const headerContainer = $(`#${ref.current.id} > .headerContainer`);
+        const projectImage = $(`#${ref.current.id}-image`);
+        const imageContainer = projectImage.parent();
+        imageContainer.removeClass('mask');
+        imageContainer.css({ overflow: 'hidden' });
+        headerContainer.animate({ opacity: 0 }, 500);
+
+        imageContainer.animate({ height: 0 }, 500, () => {
+          imageContainer.remove();
+        });
 
         setTimeout(() => {
-          $(blogPostContent).html(projectPage);
-          $(main).html(blogPostContent);
-          $(blogPostContent).fadeIn(500);
-        }, 300);
-        
+          projectPreview.animate({ width: 0 }, 500, () => {
+             $(main.firstChild).fadeOut(300, () => {
+              unmountComponentAtNode(main);
+              setTimeout(() => {
+                $(blogPostContent).html(projectPage);
+                $(main).html(blogPostContent);
+                $(blogPostContent).fadeIn(500);
+              }, 300);
+            }); 
+          });
+        }, 500);
+        });
       });
-    });
   }
 
   return (
@@ -52,17 +73,20 @@ const ProjectPreview = ({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       style={{ backgroundColor: `${color}` }}
+      id={id}
       className="projectPreviewContainer"
+      ref={ref}
     >
       <div className={`${hover ? 'maskHover' : ''} ${id} mask`}>
         <img
+          id={`${id}-image`}
           src={src}
           alt="Project mock" />
       </div>
       <div className="headerContainer">
         <h3>{title}</h3>
-          <h4>{duration}</h4>
-          <h4>{position}</h4>
+        <h4>{duration}</h4>
+        <h4>{position}</h4>
        </div>
     </div>
   );
