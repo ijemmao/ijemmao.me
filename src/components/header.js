ef999c29d37e57d0bde8b14ebe55193c36b60f6f
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-12-29 04:58:17 -0800

    Mobile menu icon with animations and navigations

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import $ from 'jquery';
 import { render } from 'react-dom';
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
@@ -12,6 +14,7 @@ import handleNavigation from '../utils/handleNavigation';
 import './header.less';
 
 const Header = ({ siteTitle, pages, height }) => {
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
 
   const renderPage = (node, label) => {
     let component;
@@ -40,14 +43,18 @@ const Header = ({ siteTitle, pages, height }) => {
       const main = document.querySelector('main');
       renderPage(main, label);
     });
-  }
 
-  const renderNavBarOptions = () => (
-    <div className="navBarOptions">
-      {map(pages, (page) => {
+    $('.pageLink').animate({ opacity: 0 }, 300, () => {
+        setIsMobileMenuVisible(false);
+    });
+  } 
+
+  const renderPages = () => (
+    map(pages, (page) => {
         const { label, route } = page;
         return (
           <Link
+            className="pageLink"
             key={label}
             to={route}
             onClick={(e) => handleRoute(e, route, label)}
@@ -56,13 +63,39 @@ const Header = ({ siteTitle, pages, height }) => {
           </Link>
         );
       })
-      }
+
+  )
+
+  const renderNavBarOptions = () => (
+    <div className="navBarOptions">
+      {renderPages()}
     </div>
-);
+  );
+
+  const renderMenuIcon = () => (
+    <div
+      className="navMobileButton"
+      onClick={() => setIsMobileMenuVisible(!isMobileMenuVisible)}
+    >
+      <div id="bar1" />
+      <div id="bar2" />
+      <div id="bar3" />
+    </div>
+  )
+
+  const renderMobileNavBarOptions = () => (
+    <div className={`navMobileBarOptions ${isMobileMenuVisible ? 'expanded' : ''}`}>
+      {renderMenuIcon()}
+      <div className="navMobileContainer">
+        {isMobileMenuVisible && renderPages()}
+      </div>
+    </div>
+  )
     
   return (
-    <header style={{ height }}>
+    <header style={{ height: !isMobileMenuVisible ? height : 'auto' }}>
       {renderNavBarOptions()}
+      {renderMobileNavBarOptions()}
     </header>
   );
 };
@@ -79,4 +112,4 @@ Header.defaultProps = {
   height: '3vh',
 }
 
-export default Header
+export default Header;
--- src/page-containers/HomeContainer/HomeContainer.js
+++ src/page-containers/HomeContainer/HomeContainer.js
@@ -13,11 +13,11 @@ const HomeContainer = () => {
   return (
     <Row className="homeContainer">
       <div className="homeContent">
-        <Col>
+        <Col className="homeTitleContainer">
           <h1>Ijemma</h1>
           <h1>Onwuzulike</h1>
         </Col>
-        <div>
+        <div className="socialLinksContainer">
           <a href="https://twitter.com/_ionwu_" target="_blank" rel="noopener noreferrer"><img id="twitter" className="socialLink" src={Twitter} alt="Twitter icon" /></a>
           <a href="https://github.com/ijemmao" target="_blank" rel="noopener noreferrer"><img id="github" className="socialLink" src={GitHub} alt="GitHub icon" /></a>
         </div>
