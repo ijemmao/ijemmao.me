f21a5bb3e36a44b9a5319a3e06c2cf1185a03f2b
ijemmao <ijemmaonwuzulike10@gmail.com>
2020-05-18 15:45:06 -0700

    Update Youtube

--- src/page-containers/HomeContainer/HomeContainer.js
+++ src/page-containers/HomeContainer/HomeContainer.js
@@ -15,11 +15,11 @@ const HomeContainer = () => {
     <div className="homeContainer">
     <h1 className="title">Ijemma Onwuzulike</h1>
     <Row className="videos">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/vHfNWsN8rTA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe type="text/html" width="560" height="315" src="https://www.youtube.com/embed/8Vlny0Z7XPA" frameborder="0"></iframe>
+      <iframe type="text/html" width="560" height="315" src="https://www.youtube.com/embed/QUssVD41DjU" frameborder="0"></iframe>
+      <iframe text="text/html" width="560" height="315" src="https://www.youtube.com/embed/vHfNWsN8rTA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Row>
       <Row className="socialLinksContainer">
-        <a href="https://www.youtube.com/channel/UC-db2t_fVN3T_kzKYSNUioA" target="_blank" rel="noopener noreferrer"><img id="youtube" className="socialLink" src={YouTube} alt="YouTube icon" /></a>
+        <a href="https://www.youtube.com/c/IjemmaOnwuzulike" target="_blank" rel="noopener noreferrer"><img id="youtube" className="socialLink" src={YouTube} alt="YouTube icon" /></a>
         <a href="https://twitter.com/ijemmaohno" target="_blank" rel="noopener noreferrer"><img id="twitter" className="socialLink" src={Twitter} alt="Twitter icon" /></a>
         <a href="https://github.com/ijemmao" target="_blank" rel="noopener noreferrer"><img id="github" className="socialLink" src={GitHub} alt="GitHub icon" /></a>
       </Row>
