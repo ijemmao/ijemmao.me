0b76a9ac5bb5a63221a06819f1d22b0b94edaa8a
Ijemma Onwuzulike <ijemmaonwuzulike@Ijemmas-MBP-2.attlocal.net>
2020-09-07 11:38:35 -0700

    Update Squarespace and Twitter links on About page

--- src/page-containers/AboutContainer/AboutContainer.js
+++ src/page-containers/AboutContainer/AboutContainer.js
@@ -9,9 +9,9 @@ const AboutContainer = () => {
   return (
     <span className="aboutContainer">
       <h2 style={{ textAlign: 'center' }}>Hello <span role="img" aria-label="Brown hand waving">👋🏾</span></h2>
-      <p>I'm Ijemma a software engineer at Squarespace in New York City.</p>
+      <p>I'm Ijemma a software engineer at <a href="https://squarespace.com/">Squarespace</a> in New York City.</p>
       <p>
-        To keep up with my day-to-day, follow me on <a href="https://twitter.com/_ionwu_">Twitter</a>.
+        To keep up with my day-to-day, follow me on <a href="https://twitter.com/ijemmaohno">Twitter</a>.
       </p>
       <p>For my professional experience, take a look at my <a href="https://www.linkedin.com/in/ijemmao/">LinkedIn</a>.</p>
     </span>
